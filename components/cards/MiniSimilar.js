12932aec451588aafb63929402fc652f4be7bb75
miki <mickeytutus2401@gmail.com>
2022-05-26 13:08:14 +0200

    change mini to webp

--- components/cards/MiniSimilar.js
+++ components/cards/MiniSimilar.js
@@ -5,11 +5,12 @@ import classes from "./MiniSimilar.module.scss";
 export default function MiniSimilar({ miniSimilarFinal }) {
   // console.log(miniSimilarFinal, "From MiniSimilar");
   // console.log(miniSimilarFinal, "From MiniSimilar");
+
   return (
     <div className={classes.cont}>
       <Link href={miniSimilarFinal.miniLink} passHref>
         <picture>
-          <source srcSet={miniSimilarFinal.miniImage} type="image/webp" />
+          <source srcSet={miniSimilarFinal.miniImageWebP} type="image/webp" />
           <img
             src={miniSimilarFinal.miniImage}
             alt={miniSimilarFinal.minAlt}
