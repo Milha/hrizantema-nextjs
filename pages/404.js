3c6ec8ee1d8a7c9130b42f2756a7cd892388cea5
miki <mickeytutus2401@gmail.com>
2022-06-03 06:47:24 +0200

    add 404 and fix in .httcess

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+import clasees from "../styles/404.module.scss";
+
+export default function PageNotFound() {
+  return (
+    <div className={clasees.cont}>
+      <h2>Nažalost stranica nije pronadjena</h2>
+      <div className={clasees.back_link}>
+        <Link href="/">
+          <a>Povratak na glavnu stranu!!!</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
