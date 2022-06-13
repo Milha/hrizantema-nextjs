2477ba78cca3aa5555cc14ff1dee2fbf2ce453f3
miki <mickeytutus2401@gmail.com>
2022-06-13 12:20:36 +0200

    add dvostranicne list

---
+++ pages/lists/list-ponuda-dvostranicne-suze.js
@@ -0,0 +1,36 @@
+import SmallCard from "../../components/cards/SmallCard";
+import HeadList from "../../components/heads/HeadList";
+
+import classes from "./listsArticles.module.scss";
+
+import { suzeDvostranicneData } from "../../data/suzeDvostranicneItemData";
+
+const reverte = suzeDvostranicneData.reverse();
+
+export default function Lists({ inject }) {
+  // console.log(inject, "from list page");
+  return (
+    <>
+      <HeadList
+        data={inject}
+        addTitle="Ponuda Suza"
+        addHttpaddress="https://hrizantema.rs/lists/list-ponuda-suze"
+      />
+      <article className={classes.cont}>
+        {inject.map((inj) => (
+          <section key={inj.id}>
+            <SmallCard data={inj}></SmallCard>
+          </section>
+        ))}
+      </article>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const inject = reverte;
+
+  return {
+    props: { inject },
+  };
+}
