de931fd1e9fda9e18bfca4145ccfbeea1a533e2a
miki <mickeytutus2401@gmail.com>
2022-06-23 09:31:49 +0200

    add meta on on products

--- components/heads/HeadSingle.js
+++ components/heads/HeadSingle.js
@@ -23,9 +23,30 @@ export default function HeadSingle({ data }) {
     },
   };
 
-  // console.log(data.httpaddress);
+  // console.log(data);
   return (
     <Head>
+      {/* TITLE */}
+      <title>Cvećara Hrizantema Orlovača | {data.title}</title>
+
+      {/* REL CANNONICAL */}
+      <link rel="canonical" href={data.httpaddress} />
+
+      {/* META */}
+      <meta name="description" content={data.text} />
+
+      {/* META:OG */}
+      <meta property="og:url" content={data.httpaddress} key="url" />
+      <meta property="og:type" content="product" key="type" />
+      <meta
+        property="og:title"
+        content={`Cvećara Hrizantema Orlovača | ${data.title}`}
+        key="title"
+      />
+      <meta property="og:description" content={data.text} key="description" />
+      <meta property="og:site_name" content="Zdrava" key="site_name"></meta>
+      <meta property="og:image" content={data.imageSmall} key="image" />
+
       {/* SCHEMA */}
       <script
         type="application/ld+json"
@@ -33,12 +54,6 @@ export default function HeadSingle({ data }) {
           __html: JSON.stringify(schemaSingleItemData),
         }}
       />
-
-      {/* META */}
-      <title>Cvećara Hrizantema Orlovača | {data.title}</title>
-
-      {/* REL CANNONICAL */}
-      <link rel="canonical" href={data.httpaddress} />
     </Head>
   );
 }
