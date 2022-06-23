275e45c22af3e2c16103160452588aa25f0a18e7
milha <shushumiga01@gmail.com>
2022-06-23 11:56:08 +0200

    add font-dispaly: swap ATM, later maybe change to optional

--- data/offersCardsData.js
+++ data/offersCardsData.js
@@ -1,23 +1,29 @@
 export const offersCardsData = [
   {
-    title: "Venci",
+    title: 'Venci',
     cardImage:
-      "/images/venci/beli-ljiljan-bele-ruze-crveni-gerber-bela-margareta-prirodni-venac-small.jpg",
-    text: "Venci od uvek svežeg cveća",
-    offerLink: "/lists/list-ponuda-venci",
+      '/images/venci/beli-ljiljan-bele-ruze-crveni-gerber-bela-margareta-prirodni-venac-small.jpg',
+    cardImageWebP:
+      '/images/venci/beli-ljiljan-bele-ruze-crveni-gerber-bela-margareta-prirodni-venac-small.webp',
+    text: 'Venci od uvek svežeg cveća',
+    offerLink: '/lists/list-ponuda-venci',
   },
   {
-    title: "Suze",
+    title: 'Suze',
     cardImage:
-      "/images/suze/roze-carski-ljiljan-orijental-prirodna-suza-small.jpg",
-    text: "Suze svih veličina i dvostranične suze",
-    offerLink: "/lists/list-ponuda-suze",
+      '/images/suze/roze-carski-ljiljan-orijental-prirodna-suza-small.jpg',
+    text: 'Suze svih veličina i dvostranične suze',
+    cardImageWebP:
+      '/images/suze/roze-carski-ljiljan-orijental-prirodna-suza-small.webp',
+    text: 'Suze svih veličina i dvostranične suze',
+    offerLink: '/lists/list-ponuda-suze',
   },
   {
-    title: "Ostalo u poundi",
-    cardImage: "/images/ostalo-u-ponudi/kucica-za-svece-velika-small.jpg",
+    title: 'Ostalo u poundi',
+    cardImage: '/images/ostalo-u-ponudi/kucica-za-svece-velika-small.jpg',
+    cardImageWebP: '/images/ostalo-u-ponudi/kucica-za-svece-velika-small.webp',
 
-    text: "Veštačko cveće, kućice za sveće, saksijsko cveće",
-    offerLink: "/lists/list-ostalo-u-ponudi",
+    text: 'Veštačko cveće, kućice za sveće, saksijsko cveće',
+    offerLink: '/lists/list-ostalo-u-ponudi',
   },
 ];
