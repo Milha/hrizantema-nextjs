98f4442f6621e97545a3fd86e2a089bdfcf10b25
milha <shushumiga01@gmail.com>
2022-06-23 13:54:29 +0200

    changed text on hero

--- components/layout/HeroBanner.js
+++ components/layout/HeroBanner.js
@@ -1,10 +1,18 @@
-import classes from "./HeroBanner.module.scss";
+import classes from './HeroBanner.module.scss';
 
 export default function HeroBanner() {
   return (
     <div className={classes.cont}>
       <div className={classes.text_cont}>
-        <p>PRELEPO CVEĆE ZA SVE VAŠE ARANŽMANE</p>
+        
+        <p>
+          Verujte nam da ćemo vam pomoći da izrazite saučešće i saosećanje,
+          pružajući utehu i podršku.
+        </p>
+        <p>
+          Cvećara Hrizantema Orlovača nudi veliki izbor cveća kojim bi preneli
+          vašu ljubav i podršku zbog gubitka voljene osobe
+        </p>
         <p>
           Prelepi aranžmani, ručno radjeni s ljubavlju. Naše cveće i buketi su
           trenutno dostupni u našoj radnji ili se isporučuju istog ili sledećeg
