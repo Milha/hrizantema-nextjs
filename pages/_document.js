761ee83dd069d49202b341e1f443ab9f60f4b83b
miki <mickeytutus2401@gmail.com>
2022-07-09 18:52:50 +0200

    kick link rel=preconnect

--- pages/_document.js
+++ pages/_document.js
@@ -8,13 +8,20 @@ class MyDocument extends Document {
           {/* GOOGLE FONTS */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
 
-          <link
+          {/* <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
             crossOrigin="true"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Marcellus&display=swap"
+            rel="stylesheet"
+          /> */}
+
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
+          {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300;400;500;600;700;800&family=Merriweather:wght@300;400;700;900&display=swap"
             rel="stylesheet"
           />
         </Head>
