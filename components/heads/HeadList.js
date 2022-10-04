010804a20ba2a8d56af4597536b684442fbf404c
miki <mickeytutus2401@gmail.com>
2022-10-04 12:09:39 +0200

    add h2 to list was missing, and metaOG to list

--- components/heads/HeadList.js
+++ components/heads/HeadList.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-export default function HeadList({ data, addTitle, addHttpaddress }) {
+export default function HeadList({ data, addTitle, addHttpaddress, listDesc }) {
   const eleMap = data.map((inj, i) => ({
     "@type": "ListItem",
     position: i + 1,
@@ -15,6 +15,10 @@ export default function HeadList({ data, addTitle, addHttpaddress }) {
     itemListElement: eleMap,
   };
 
+  const ogImage = data[0].imageGMB;
+
+  // console.log(ogImage);
+
   return (
     <Head>
       {/* SCHEMA */}
@@ -28,6 +32,26 @@ export default function HeadList({ data, addTitle, addHttpaddress }) {
       <title>{`Cvećara Hrizantema Orlovača | ${addTitle}`}</title>
       {/* REL CANNONICAL */}
       <link rel="canonical" href={addHttpaddress} />
+
+      {/* META:OG */}
+      <meta property="og:url" content={addHttpaddress} key="url" />
+      <meta property="og:type" content="product list" key="type" />
+      <meta
+        property="og:title"
+        content={`Cvećara Orlovača | ${addTitle}`}
+        key="title"
+      />
+      <meta property="og:description" content={listDesc} key="description" />
+      <meta
+        property="og:site_name"
+        content="Hrizantema cvećara Orlovača"
+        key="site_name"
+      ></meta>
+      <meta
+        property="og:image"
+        content={`https://hrizantema.rs${ogImage}`}
+        key="image"
+      />
     </Head>
   );
 }
