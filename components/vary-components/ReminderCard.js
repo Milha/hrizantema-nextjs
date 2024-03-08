06c1dd7e377548d4f61a5ec2aed7a01be0a00bed
Milha <mickeytuts2401@gmail.com>
2024-03-08 06:41:18 +0100

    change minor text in card

--- components/vary-components/ReminderCard.js
+++ components/vary-components/ReminderCard.js
@@ -5,7 +5,7 @@ const ReminderCard = () => {
     <div className={classes.cont}>
       <p className={classes.text}>
         Podsetnik za datum zadusnica na groblju Orlovača i drugim beogradskim
-        grobljima: 09.03.2024 Zadušnice
+        grobljima: 09.03.2024 Zadušnice - Zimske zadušnice
       </p>
     </div>
   );
