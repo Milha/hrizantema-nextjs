e6e099f1a05b5ee99d52059993269f9a7473f2ef
Miki <mickeytuts2401@gmail.com>
2024-11-05 20:12:36 +0100

    change date of zadusnice

--- components/vary-components/ReminderCard.js
+++ components/vary-components/ReminderCard.js
@@ -14,7 +14,7 @@ const ReminderCard = () => {
     <div className={classes.cont}>
       <p className={classes.text}>
         Podsetnik za datum zadušnica na groblju Orlovača i drugim beogradskim
-        grobljima: 02.11.2024 Mitrovske jesenje zadušnice
+        grobljima: 22.02.2025 Zimske zadušnice
       </p>
       <hr></hr>
       <p className={classes.text}>{reccenica}</p>
