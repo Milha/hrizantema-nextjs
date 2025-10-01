a2ca85c5dd9b98786bfabd8c081b9bc7ee0311f7
Miki <mickeytuts2401@gmail.com>
2025-10-01 11:19:06 +0200

    update to NextJS 15 and scss

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   experimental: {
     scrollRestoration: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
