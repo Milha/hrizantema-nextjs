331f6d8a3b54c97c97d5d4a12a305683d8c20d31
Miki <mickeytuts2401@gmail.com>
2025-10-03 15:47:42 +0200

    add to qands-data.js

--- data/qandas-data.js
+++ data/qandas-data.js
@@ -11,9 +11,13 @@ export const qandasData = [
       " Ne. Aranžmani su samo neki od mnogih koje smo izradili. Svaki aranžman može biti veći ili manji od prikazanih, druge boje cveća i slično. Za sve može dogovor.",
   },
   {
-    question:
-      "Koje je radno vreme groblja Orlovača?",
+    question: "Koje je radno vreme groblja Orlovača?",
     answer:
       "Kapija na groblju Orlovača je otvorena od 07.00 do 19.00 u letnjem periodu (01.04 - 30.09), od 07.00 do 18.00 u zimskom periodu (01.10 - 31.03).",
+  },
+  {
+    question: "Koliko traje izrada aranžmana?",
+    answer:
+      "Pored uvek dostupnih aranžmana, sama izrada aranžmana po Vašoj želji traje od 20 do 60 minuta, u zavisnosti od veličine i složenosti aranžmana. Obzirmo na specifičnost porudžbina cvetnih aranžmana, preporučujemo da porudžbine vršite najmanje dan ranije, kako bismo izradili aranžman koji najbolje ispunjava Vaše želje i obezbedili dostupnost cveća koje želite.",
   },
 ];
