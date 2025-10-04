1e58192228d6221fd580dacd063d2d63a51dc3a7
Miki <mickeytuts2401@gmail.com>
2025-10-04 17:33:34 +0200

    add working footer

--- components/layout/Footer/index.js
+++ components/layout/Footer/index.js
@@ -1,14 +1,11 @@
 import classes from "./Footer.module.scss";
+import Link from "next/link";
 
 function Footer() {
   return (
     <footer className={classes["footer-container"]}>
       <div className={classes["footer-content"]}>
-        <div className={classes["footer-left"]}>
-          <h3>Hrizantema</h3>
-          <p>© {new Date().getFullYear()} Sva prava zadržava hrizantema.</p>
-        </div>
-        <div className={classes["footer-center"]}>
+        <div className={classes["footer-contact"]}>
           <address className={classes["footer-address"]}>
             <strong>Hrizantema</strong>
             <br />
@@ -22,11 +19,39 @@ function Footer() {
               kontakt.sztrhrizantema@gmail.com
             </a>
           </address>
+          <p>PIB: 111817197</p>
+          <p>Matični broj: 65682478</p>
+        </div>
+        <div className={classes["footer-links"]}>
+          <h4>Navigacija</h4>
+          <ul>
+            <li>
+              <Link href="/lists/list-ponuda-venci">Venci</Link>
+            </li>
+            <li>
+              <Link href="/lists/list-ponuda-suze">Klasične suze</Link>
+            </li>
+            <li>
+              <Link href="/lists/list-ponuda-dvostranicne-suze">
+                Dvostranične suze
+              </Link>
+            </li>
+            <li>
+              <Link href="/lists/list-ponuda-buketi-korpe">Buketi</Link>
+            </li>
+            <li>
+              <Link href="/lists/lista-ponuda-rezano-cvece">Rezano cveće</Link>
+            </li>
+            <li>
+              <Link href="/lists/list-ostalo-u-ponudi">Ostalo u ponudi</Link>
+            </li>
+          </ul>
           {/* <a href="/about">O nama</a>
           <a href="/contact">Kontakt</a>
           <a href="/privacy">Privatnost</a> */}
         </div>
-        <div className={classes["footer-right"]}>
+
+        <div className={classes["footer-about"]}>
           <h4>O nama</h4>
           <p>
             Cvećara Hrizantema, koja se nalazi na groblju Orlovača, posluje još
@@ -34,6 +59,11 @@ function Footer() {
             aranžmana i posvećeni smo pružanju najkvalitetnije usluge.
           </p>
         </div>
+      </div>
+
+      <div className={classes["footer-rights"]}>
+        <h3>Hrizantema</h3>
+        <p>© {new Date().getFullYear()} Sva prava zadržava hrizantema.</p>
       </div>
     </footer>
   );
