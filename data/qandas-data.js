433cc4a64cdb691cbffe6dc22b8168c83b208b34
Miki <mickeytuts2401@gmail.com>
2025-10-21 19:03:13 +0200

    new qanda

--- data/qandas-data.js
+++ data/qandas-data.js
@@ -16,8 +16,8 @@ export const qandasData = [
       "Kapija na groblju Orlovača je otvorena od 07.00 do 19.00 u letnjem periodu (01.04 - 30.09), od 07.00 do 18.00 u zimskom periodu (01.10 - 31.03).",
   },
   {
-    question: "Koliko traje izrada aranžmana?",
+    question: "Koji su datumi zadušnica?",
     answer:
-      "Pored uvek dostupnih aranžmana, sama izrada aranžmana po Vašoj želji traje od 20 do 60 minuta, u zavisnosti od veličine i složenosti aranžmana. Obzirmo na specifičnost porudžbina cvetnih aranžmana, preporučujemo da porudžbine vršite najmanje dan ranije, kako bismo izradili aranžman koji najbolje ispunjava Vaše želje i obezbedili dostupnost cveća koje želite.",
+      "Na našoj naslovnoj stranici možete naći datum sledećih zadušnica na groblju Orlovača, ostalim beogradskim grobljima i pravoslavnim grobljima u Srbiji pod okriljem Srpske pravoslavne crkve.",
   },
 ];
