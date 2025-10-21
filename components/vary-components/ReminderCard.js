b4a3a7a7f805bfc230a7c04487764ea74157a680
Miki <mickeytuts2401@gmail.com>
2025-10-21 18:28:12 +0200

    change dates

--- components/vary-components/ReminderCard.js
+++ components/vary-components/ReminderCard.js
@@ -1,20 +1,21 @@
 import classes from "./ReminderCard.module.scss";
 
-
 const ReminderCard = () => {
-  let currMonth =  new Date();
-  let month =  currMonth.getMonth() + 1;
+  let currMonth = new Date();
+  let month = currMonth.getMonth() + 1;
 
-  
-  let reccenica = (month >= 3 && month <= 9 ) ? "Kapija na groblju Orlovača je otvorena od 07.00 do 19.00 - letnji period od 01.04 do 30.09 " : "Kapija na groblju Orlovača je otvorena od 07.00 do 18.00 - zimski period od 01.10 do 31.03";
-  
+  let reccenica =
+    month >= 3 && month <= 9
+      ? "Kapija na groblju Orlovača je otvorena od 07.00 do 19.00 - letnji period od 01.04 do 30.09 "
+      : "Kapija na groblju Orlovača je otvorena od 07.00 do 18.00 - zimski period od 01.10 do 31.03";
+
   // console.log(reccenica);
 
   return (
     <div className={classes.cont}>
       <p className={classes.text}>
         Podsetnik za datum zadušnica na groblju Orlovača i drugim beogradskim
-        grobljima: 11.10.2025 Miholjske zadušnice
+        grobljima: 01.11.2025 Mitrovske zadušnice
       </p>
       <hr></hr>
       <p className={classes.text}>{reccenica}</p>
