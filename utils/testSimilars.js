34532af9b1b100501e13e3582877b627b35b9786
Miki <mickeytuts2401@gmail.com>
2025-11-03 12:19:27 +0100

    refactor testSimilaras

--- utils/testSimilars.js
+++ utils/testSimilars.js
@@ -71,65 +71,101 @@ export const simBuketiTest = buketiKorpeItemData.map((inj) => ({
   ],
 }));
 
-export const simVenciDataFind = venciItemData.map((inj) => ({
-  sluglink: inj.sluglink,
-  similar: [
-    {
-      miniSimilarLink: venciItemData.find((el) => el.title === inj.similarTo[0])
-        .link,
-      miniSimilarTitle: venciItemData.find(
-        (el) => el.title === inj.similarTo[0]
-      ).title,
-      miniSimilarAlt: venciItemData.find((el) => el.title === inj.similarTo[0])
-        .alt,
-      miniSimilarPrice: venciItemData.find(
-        (el) => el.title === inj.similarTo[0]
-      ).price,
-      miniImageSimilar:
-        venciItemData.find((el) => el.title === inj.similarTo[0]).imageMini ??
-        "",
-      miniImageWebPSimilar:
-        venciItemData.find((el) => el.title === inj.similarTo[0])
-          .imageMiniWebP ?? "",
-    },
-    {
-      miniSimilarLink: venciItemData.find((el) => el.title === inj.similarTo[1])
-        .link,
-      miniSimilarTitle: venciItemData.find(
-        (el) => el.title === inj.similarTo[1]
-      ).title,
-      miniSimilarAlt: venciItemData.find((el) => el.title === inj.similarTo[1])
-        .alt,
-      miniSimilarPrice: venciItemData.find(
-        (el) => el.title === inj.similarTo[1]
-      ).price,
-      miniImageSimilar:
-        venciItemData.find((el) => el.title === inj.similarTo[1]).imageMini ??
-        "",
-      miniImageWebPSimilar:
-        venciItemData.find((el) => el.title === inj.similarTo[1])
-          .imageMiniWebP ?? "",
-    },
-    {
-      miniSimilarLink: venciItemData.find((el) => el.title === inj.similarTo[2])
-        .link,
-      miniSimilarTitle: venciItemData.find(
-        (el) => el.title === inj.similarTo[2]
-      ).title,
-      miniSimilarAlt: venciItemData.find((el) => el.title === inj.similarTo[2])
-        .alt,
-      miniSimilarPrice: venciItemData.find(
-        (el) => el.title === inj.similarTo[2]
-      ).price,
-      miniImageSimilar:
-        venciItemData.find((el) => el.title === inj.similarTo[2]).imageMini ??
-        "",
-      miniImageWebPSimilar:
-        venciItemData.find((el) => el.title === inj.similarTo[2])
-          .imageMiniWebP ?? "",
-    },
-  ],
-}));
+// export const simVenciDataFind = venciItemData.map((inj) => ({
+//   sluglink: inj.sluglink,
+//   similar: [
+//     {
+//       miniSimilarLink: venciItemData.find((el) => el.title === inj.similarTo[0])
+//         .link,
+//       miniSimilarTitle: venciItemData.find(
+//         (el) => el.title === inj.similarTo[0]
+//       ).title,
+//       miniSimilarAlt: venciItemData.find((el) => el.title === inj.similarTo[0])
+//         .alt,
+//       miniSimilarPrice: venciItemData.find(
+//         (el) => el.title === inj.similarTo[0]
+//       ).price,
+//       miniImageSimilar:
+//         venciItemData.find((el) => el.title === inj.similarTo[0]).imageMini ??
+//         "",
+//       miniImageWebPSimilar:
+//         venciItemData.find((el) => el.title === inj.similarTo[0])
+//           .imageMiniWebP ?? "",
+//     },
+//     {
+//       miniSimilarLink: venciItemData.find((el) => el.title === inj.similarTo[1])
+//         .link,
+//       miniSimilarTitle: venciItemData.find(
+//         (el) => el.title === inj.similarTo[1]
+//       ).title,
+//       miniSimilarAlt: venciItemData.find((el) => el.title === inj.similarTo[1])
+//         .alt,
+//       miniSimilarPrice: venciItemData.find(
+//         (el) => el.title === inj.similarTo[1]
+//       ).price,
+//       miniImageSimilar:
+//         venciItemData.find((el) => el.title === inj.similarTo[1]).imageMini ??
+//         "",
+//       miniImageWebPSimilar:
+//         venciItemData.find((el) => el.title === inj.similarTo[1])
+//           .imageMiniWebP ?? "",
+//     },
+//     {
+//       miniSimilarLink: venciItemData.find((el) => el.title === inj.similarTo[2])
+//         .link,
+//       miniSimilarTitle: venciItemData.find(
+//         (el) => el.title === inj.similarTo[2]
+//       ).title,
+//       miniSimilarAlt: venciItemData.find((el) => el.title === inj.similarTo[2])
+//         .alt,
+//       miniSimilarPrice: venciItemData.find(
+//         (el) => el.title === inj.similarTo[2]
+//       ).price,
+//       miniImageSimilar:
+//         venciItemData.find((el) => el.title === inj.similarTo[2]).imageMini ??
+//         "",
+//       miniImageWebPSimilar:
+//         venciItemData.find((el) => el.title === inj.similarTo[2])
+//           .imageMiniWebP ?? "",
+//     },
+//   ],
+// }));
+
+export const simVenciDataFind = venciItemData.map((inj) => {
+  const sim0 = venciItemData.find((el) => el.title === inj.similarTo[0]);
+  const sim1 = venciItemData.find((el) => el.title === inj.similarTo[1]);
+  const sim2 = venciItemData.find((el) => el.title === inj.similarTo[2]);
+
+  return {
+    sluglink: inj.sluglink,
+    similar: [
+      {
+        miniSimilarLink: sim0?.link ?? "",
+        miniSimilarTitle: sim0?.title ?? "",
+        miniSimilarAlt: sim0?.alt ?? "",
+        miniSimilarPrice: sim0?.price ?? "",
+        miniImageSimilar: sim0?.imageMini ?? "",
+        miniImageWebPSimilar: sim0?.imageMiniWebP ?? "",
+      },
+      {
+        miniSimilarLink: sim1?.link ?? "",
+        miniSimilarTitle: sim1?.title ?? "",
+        miniSimilarAlt: sim1?.alt ?? "",
+        miniSimilarPrice: sim1?.price ?? "",
+        miniImageSimilar: sim1?.imageMini ?? "",
+        miniImageWebPSimilar: sim1?.imageMiniWebP ?? "",
+      },
+      {
+        miniSimilarLink: sim2?.link ?? "",
+        miniSimilarTitle: sim2?.title ?? "",
+        miniSimilarAlt: sim2?.alt ?? "",
+        miniSimilarPrice: sim2?.price ?? "",
+        miniImageSimilar: sim2?.imageMini ?? "",
+        miniImageWebPSimilar: sim2?.imageMiniWebP ?? "",
+      },
+    ],
+  };
+});
 
 export const simDvostranicneSuzeTest = suzeDvostranicneData.map((inj) => ({
   sluglink: inj.sluglink,
