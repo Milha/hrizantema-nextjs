7219075553a526088f63f75ea694dca15fef77c5
Miki <mickeytuts2401@gmail.com>
2025-12-02 12:12:27 +0100

    add cancel button next to select

--- components/ui/searchs/SelectSearch.tsx
+++ components/ui/searchs/SelectSearch.tsx
@@ -1,7 +1,5 @@
 import classes from "./SelectSearch.module.scss";
 
-
-
 const SelectSearch = ({ selectedFlower, setSelectedFlower, flowerOptions }) => {
   return (
     <>
@@ -9,18 +7,28 @@ const SelectSearch = ({ selectedFlower, setSelectedFlower, flowerOptions }) => {
         <label htmlFor="flowerOption" className="block mb-2 font-medium">
           Pretraži po sadržaju:
         </label>
-        <select
-          id="flowerOption"
-          value={selectedFlower}
-          onChange={(e) => setSelectedFlower(e.target.value)}
-        >
-          <option value="">Izaberite cvet</option>
-          {flowerOptions.map((flower) => (
-            <option key={flower} value={flower}>
-              {flower}
-            </option>
-          ))}
-        </select>
+        <div className={classes.selectWithButton}>
+          <select
+            id="flowerOption"
+            value={selectedFlower}
+            onChange={(e) => setSelectedFlower(e.target.value)}
+          >
+            <option value="">Izaberite cvet</option>
+            {flowerOptions.map((flower) => (
+              <option key={flower} value={flower}>
+                {flower}
+              </option>
+            ))}
+          </select>
+          {selectedFlower && (
+            <button
+              className={classes.cancelButton}
+              onClick={() => setSelectedFlower("")}
+            >
+              X
+            </button>
+          )}
+        </div>
       </div>
     </>
   );
