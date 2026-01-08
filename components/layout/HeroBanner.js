c36de89499d622f9cf6fef34e887536307dcd833
Miki <mickeytuts2401@gmail.com>
2026-01-08 11:33:17 +0100

    cleaning HeroBanner files

--- components/layout/HeroBanner.js
+++ components/layout/HeroBanner.js
@@ -4,14 +4,13 @@ import currentContent from "../../data/helpers/currentBannerContent";
 
 export default function HeroBanner() {
   return (
-    <div className={classes.cont}>
+    <section className={classes.cont}>
       <div className={classes.text_cont}>
-        {/* NEW VERSION 01. 2026 */}
         {currentContent.paragraphs.map((paragraph, index) => (
           <p key={index}>{paragraph}</p>
         ))}
       </div>
       <div className={classes.image_cont}></div>
-    </div>
+    </section>
   );
 }
