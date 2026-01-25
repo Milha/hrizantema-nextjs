3a10114fff93e5352125fdadc5d39d67f8629ce1
Miki <mickeytuts2401@gmail.com>
2026-01-25 17:25:02 +0100

    fetch data and start fromat page for display schedual

--- pages/unique/schedule-ceremony.tsx
+++ pages/unique/schedule-ceremony.tsx
@@ -1,7 +1,36 @@
 import React from "react";
 
+import ceremonies from "../../data/ceremony/ceremonies.json";
+
 const ScheduleCeremony = () => {
-  return <div>ScheduleCeremony</div>;
+  const cleanedCeremony = ceremonies.ceremonies.map((cer) => {
+    return cer.ceremonies;
+  });
+  console.log(cleanedCeremony);
+  return (
+    <div>
+      {ceremonies.ceremonies.map((cer) => {
+        return (
+          <div key={cer._id}>
+            {cer.ceremonies.map((c) => {
+              return (
+                <div key={c._id}>
+                  <p>
+                    {new Date(c.ceremonyDate).toLocaleString("sr-RS", {
+                      dateStyle: "short",
+                      timeStyle: "short",
+                    })}
+                  </p>
+                  <p>{c.deceased.firstName}</p>
+                  <p>{c.deceased.lastName}</p>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ScheduleCeremony;
